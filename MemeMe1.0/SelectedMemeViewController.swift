0c76ade90784f42bbf8ae3937c7ffab2d29969f6
Celia Gómez de Villavedón Pedrosa <celiagomezdev@gmail.com>
2017-05-04 12:14:57 +0200

    Configure working version of SelectedMemeViewController

--- MemeMe1.0/SelectedMemeViewController.swift
+++ MemeMe1.0/SelectedMemeViewController.swift
@@ -11,27 +11,15 @@ import UIKit
 class SelectedMemeViewController: UIViewController {
     
     var meme: Meme!
+    @IBOutlet weak var memedImage: UIImageView!
 
     override func viewDidLoad() {
+         // Do any additional setup after loading the view.
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.memedImage.image = meme.memedImage!
     }
-    */
-
 }
